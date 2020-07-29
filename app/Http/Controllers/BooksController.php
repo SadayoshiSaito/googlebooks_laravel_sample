7ed6aa70f0e2a50b2603adf0d734ef63a858eb4d
SadayoshiSaito <sadapi_100@yahoo.co.jp>
2020-07-29 22:35:30 +0900

    last

--- app/Http/Controllers/BooksController.php
+++ app/Http/Controllers/BooksController.php
@@ -4,15 +4,48 @@ namespace GoogleBooksSample\Http\Controllers;
 
 use Illuminate\Http\Request;
 
+//Clientクラスを使用する
+use GuzzleHttp\Client;
+
 class BooksController extends Controller
 {
-    //
+    /**
+     * 一覧表示
+     */
     public function index(Request $request)
     {
-        //
         $data = [];
+        
+        $items = null;
 
+        if (!empty($request->keyword))
+        {
+            // 検索キーワードあり
 
+            // 日本語で検索するためにURLエンコードする
+            $title = urlencode($request->keyword);
+
+            // APIを発行するURLを生成
+            $url = 'https://www.googleapis.com/books/v1/volumes?q=' . $title . '&country=JP&orderBy=newest&tbm=bks';
+    
+            $client = new Client();
+
+            // GETでリクエスト
+            $response = $client->request("GET", $url);
+    
+            $body = $response->getBody();
+            
+            // JSON形式を連想配列にデコード
+            $bodyArray = json_decode($body, true);
+    
+            // 書籍情報部分を取得
+            $items = $bodyArray['items'];
+        }
+
+        $data = [
+            'items' => $items,
+            'keyword' => $request->keyword,
+        ];
 
         return view('index', $data);
     }
--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -2,9 +2,28 @@
 <html lang="ja">
     <head>
         <meta charset="UTF-8">
-        <title>タイトル</title>
+        <title>PHP/LaravelでGoogle books apiを使うサンプル</title>
     </head>
     <body>
-        <p>本文です。</p>
+        <h1>PHP/LaravelでGoogle books apiを使うサンプル</h1>
+        <form action="/" method="get">
+            書籍名:<input type="text" name="keyword" size="50" value="{{ $keyword }}">&nbsp;<input type="submit" value="検索">
+        </form>
+
+        @if ($items == null)
+            <p>書籍名を入力してください。</p>
+        @else (count($items) > 0)
+            <p>検索語：{{ $keyword }}</p>
+            <hr>
+            @foreach ($items as $item)
+                <img src="{{ $item['volumeInfo']['imageLinks']['thumbnail']}}"><br>
+                タイトル：{{ $item['volumeInfo']['title']}}<br>
+                著者：{{ $item['volumeInfo']['authors'][0]}}<br>
+                発売年月：{{ $item['volumeInfo']['publishedDate']}}<br>
+                <br>
+                概要：{{ $item['volumeInfo']['description']}}<br>
+                <hr>
+            @endforeach
+        @endif
     </body>
 </html>
